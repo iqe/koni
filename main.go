ca640c18e8830f34735b39ed9b48e6cce1d6e7de
Michael Goth <mg@webflow.de>
2018-09-24 13:43:18 +0200

    Use a configuration file instead of command line args

--- main.go
+++ main.go
@@ -8,21 +8,23 @@ import (
 	"time"
 
 	"github.com/go-macaron/pongo2"
+	"github.com/pelletier/go-toml"
 	"gopkg.in/macaron.v1"
 )
 
 var (
-	addrFlag       = flag.String("l", "127.0.0.1:4443", "IP and port to listen on (HTTPs only)")
-	urlFlag        = flag.String("u", "https://acme-staging.api.letsencrypt.org/directory", "Let's Encrypt URL")
-	certsDirFlag   = flag.String("c", ".", "Certificate cache directory where certs are stored")
-	smtpServerFlag = flag.String("s", "smtp.webflow.de", "SMTP server")
-	imapServerFlag = flag.String("i", "imap.webflow.de", "IMAP server")
-	popServerFlag  = flag.String("p", "pop.webflow.de", "POP server")
+	configFileFlag = flag.String("c", "koni.conf", "Path to configuration file")
+)
+
+const (
+	defaultListen   = "127.0.0.1:4443"
+	defaultURL      = "https://acme-staging.api.letsencrypt.org/directory"
+	defaultCertsDir = "."
 )
 
 var (
-	email      = "administrator@webflow.de"
-	provider   = "webflow"
+	email      string
+	provider   string
 	smtpServer string
 	imapServer string
 	popServer  string
@@ -30,12 +32,22 @@ var (
 
 func main() {
 	flag.Parse()
-	addrs := *addrFlag
-	url := *urlFlag
-	certsDir := *certsDirFlag
-	smtpServer = *smtpServerFlag
-	imapServer = *imapServerFlag
-	popServer = *popServerFlag
+	configFile := *configFileFlag
+	config, err := toml.LoadFile(configFile)
+	if err != nil {
+		log.Fatalf("Failed to load config file %s: %s", configFile, err)
+	}
+
+	addr := config.GetDefault("listen", defaultListen).(string)
+
+	url := config.GetDefault("letsencrypt.url", defaultURL).(string)
+	certsDir := config.GetDefault("letsencrypt.certs_dir", defaultCertsDir).(string)
+	email := config.Get("letsencrypt.email").(string)
+
+	provider = config.Get("mail.provider_id").(string)
+	imapServer = config.Get("mail.imap_server").(string)
+	popServer = config.Get("mail.pop3_server").(string)
+	smtpServer = config.Get("mail.smtp_server").(string)
 
 	// Remove date + time from logging output (systemd adds those for us)
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
@@ -63,7 +75,7 @@ func main() {
 	manager := buildAutocertManager(url, email, certsDir)
 
 	s := &http.Server{
-		Addr:         addrs,
+		Addr:         addr,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second, // Enough time to handle Let's Encrypt challenge on first request for any domain
 		IdleTimeout:  120 * time.Second,
@@ -78,6 +90,6 @@ func main() {
 	log.Printf("IMAP server: %s\n", imapServer)
 	log.Printf("POP3 server: %s\n", popServer)
 
-	log.Printf("Starting HTTPS server on %s\n", addrs)
+	log.Printf("Starting HTTPS server on %s\n", addr)
 	log.Println(s.ListenAndServeTLS("", ""))
 }
