2c8401e4688474222509c6b5bfb8f5507ed66770
Michael Goth <mg@webflow.de>
2018-09-26 13:40:27 +0200

    Disable left-over debugging code that allowed all hostnames
    This caused lots of noise in the logs

--- autocert.go
+++ autocert.go
@@ -15,7 +15,7 @@ var (
 
 func buildAutocertManager(letsEncryptURL string, email string, certsDir string) *autocert.Manager {
 	hostPolicy := func(ctx context.Context, host string) error {
-		if hostRegex.MatchString(host) || true {
+		if hostRegex.MatchString(host) {
 			return nil
 		}
 		return fmt.Errorf("acme/autocert: invalid hostname: %s, only autoconfig/autodiscover hosts are allowed", host)
