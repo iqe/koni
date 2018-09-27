a3af78c6a3c998d0ba8175c08c46aee454e7f4ba
Michael Goth <mg@webflow.de>
2018-09-27 10:16:35 +0200

    Allow server's hostname in host policy
    This is required because clients handle CNAME + TLS SNI differently

--- autocert.go
+++ autocert.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 	"regexp"
 
 	"golang.org/x/crypto/acme"
@@ -14,11 +16,24 @@ var (
 )
 
 func buildAutocertManager(letsEncryptURL string, email string, certsDir string) *autocert.Manager {
+	myHostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("koni: failed to get local hostname from OS: %s\n", err)
+	}
+
 	hostPolicy := func(ctx context.Context, host string) error {
-		if hostRegex.MatchString(host) {
+		// Match both autoconfig.*/autodiscover.* and the real hostname because clients
+		// handle CNAMEs differently. Given the following CNAME entry:
+		//
+		// CNAME autoconfig.userdomain.com -> koniserver.provider.com
+		//
+		// * Thunderbird uses koniserver.provider.com in TLS SNI
+		// * Curl uses autoconfig.userdomain.com in TLS SNI
+		//
+		if hostRegex.MatchString(host) || host == myHostname {
 			return nil
 		}
-		return fmt.Errorf("acme/autocert: invalid hostname: %s, only autoconfig/autodiscover hosts are allowed", host)
+		return fmt.Errorf("koni: Hostname %s not allowed by host policy", host)
 	}
 
 	return &autocert.Manager{
